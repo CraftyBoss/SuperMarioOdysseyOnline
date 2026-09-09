40a7bc5bdae33494f5e0109f6b509e9edcdc15fe
2Fast2Cool <2Fast2Col@gmail.com>
2026-09-08 19:41:06 -0500

    Remove Poof Particles

--- source/puppets/PuppetHolder.cpp
+++ source/puppets/PuppetHolder.cpp
@@ -84,11 +84,9 @@ void PuppetHolder::update() {
         if (curInfo->isInSameStage && al::isDead(curPuppet)) {
             curPuppet->makeActorAlive();
 
-            curPuppet->emitJoinEffect();
         } else if (!curInfo->isInSameStage && !al::isDead(curPuppet)) {
             curPuppet->makeActorDead();
 
-            curPuppet->emitJoinEffect();
         }
     }
 }
